a103eea43ba50e603eecbc914296bcd799fc25b0
이후기 <lugan@naver.com>
2023-07-25 04:33:39 +0900

    * CompositionLocal 샘플 코드 작성

--- app/src/main/java/com/example/compolocaldemo/MainActivity.kt
+++ app/src/main/java/com/example/compolocaldemo/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.compolocaldemo
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
@@ -24,28 +27,28 @@ class MainActivity : ComponentActivity() {
             CompoLocalDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-
+                    Composable1()
                 }
             }
         }
     }
 }
 
-
+// 상태 전역변수
+val LocalColor = compositionLocalOf { Color(0xFFffdbcf) }
 
 @Composable
 fun Composable1() {
-    val LocalColor = compositionLocalOf { Color.Red }
-    val color = Color.Blue
-
-    CompositionLocalProvider(LocalColor provides color) {
-        Composable5()
-    }
-
+    // 다크테마인지 아닌지에 따라 컬러 변경
+    var color = if(isSystemInDarkTheme()) Color(0xFFa08d87) else Color(0xFFffdbcf)
 
     Column {
         Composable2()
-        Composable3()
+
+        // 전역 상태변수 제공
+        CompositionLocalProvider(LocalColor provides color){
+            Composable3()
+        }
     }
 }
 
@@ -56,7 +59,12 @@ fun Composable2() {
 
 @Composable
 fun Composable3() {
-    Composable5()
+    Text(modifier = Modifier.background(LocalColor.current), text = "Composable 3")
+
+    // Composable 5 에 Red 전역 상태 제공
+    CompositionLocalProvider(LocalColor provides Color.Red) {
+        Composable5()
+    }
 }
 
 @Composable
@@ -66,23 +74,40 @@ fun Composable4() {
 
 @Composable
 fun Composable5() {
+    Text(modifier = Modifier.background(LocalColor.current), text = "Composable 5")
 
-    Composable7()
-    Composable8()
+    // Composable 7 에 Green 전역 상태 제공
+    CompositionLocalProvider(LocalColor provides Color.Green) {
+        Composable7()
+    }
+
+    // Composable 8 에 Yellow 전역 상태 제공
+    CompositionLocalProvider(LocalColor provides Color.Yellow) {
+        Composable8()
+    }
 }
 
 @Composable
 fun Composable6() {
-    Text(text = "Composable 6")
+    // Composable 6 는 2 -> 4 -> 6 트리의 다른 브랜치에 있어서 전역 상태변수에 현재값에 접근할수 없어서 기본값을 사용한다.
+    Text(modifier = Modifier.background(LocalColor.current), text = "Composable 6")
 }
 
 @Composable
 fun Composable7() {
-
+    Text(modifier = Modifier.background(LocalColor.current), text = "Composable 7")
 }
 
 @Composable
 fun Composable8() {
+    Text(modifier = Modifier.background(LocalColor.current), text = "Composable 8")
+}
 
-    Text(text = "Composable 8")
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DarkPreview() {
+    CompoLocalDemoTheme {
+        Composable1()
+    }
 }
